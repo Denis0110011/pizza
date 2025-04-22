0d5ce22b39058101f56da0afa7508d7c97156308
DENIER <ozhegov.den0110011@gmail.com>
2025-04-23 00:22:02 +0300

    AdminMiddleware

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -23,26 +23,6 @@ final class ProductController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create(): void {}
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request): Product
-    {
-        $validated = $request->validate([
-            'name' => 'required|string|min:3|max:100',
-            'description' => 'nullable|string|min:3|max:500',
-            'price' => 'required|numeric|min:1',
-            'type' => 'required|in:pizza,drink',
-        ]);
-
-        return Product::create($validated);
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(string $id): Product
@@ -50,35 +30,6 @@ final class ProductController extends Controller
         return Product::find($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id): void {}
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Product $product): Product
-    {
-        $validated = $request->validate([
-            'name' => 'sometimes|string|min:3|max:100',
-            'description' => 'nullable|string|min:3|max:500',
-            'price' => 'sometimes|numeric|min:1',
-            'type' => 'sometimes|in:pizza,drink',
-        ]);
-        $product->update($validated);
 
-        return $product;
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(int $id): Response
-    {
-        $product = Product::find($id);
-        $product->delete();
-
-        return response()->noContent();
-    }
 }
